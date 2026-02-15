fa98fe05ae6ce7c8cde3ab2cee2fd96591e542dd
infotl756 <thoms35bzh@gmail.com>
2026-02-15 17:14:05 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,46 +1,61 @@
-let diviseur a b = match (a mod b) with 
- | 0 -> true
- | _ -> false;;
+let expo_rapide_terminale x n = 
+    let rec aux acc x n =
+        if n = 0 then acc else match n mod 2 with
+            | 0 -> aux acc (x*x) (n/2)
+            | _ -> aux (acc*x) (x*x) ((n-1)/2)
+    in aux 1 x n;;
 
-let maximum a b = match (a > b) with 
- | true -> a
- | _ -> b;;
+let c1 = expo_rapide_terminale 5 3;;
 
-let compose f g x = f (g x);;
+let syracuse n p = 
+    let rec aux acc n =
+        if n = 0 then acc else match acc mod 2 with
+            | 0 -> aux (acc/2) (n-1)
+            | _ -> aux (3*acc+1) (n-1)
+    in aux p n;;
 
-let carre x = x*x;;
-let affine x = 3*x;;
+let c2 = syracuse 9 87;;
 
-let c = compose carre affine 3;;
+let affiche n p =
+    let n0 = n in    
+        let rec aux acc n =
+                
+                print_string "Le terme u_";
+                print_int (n0-n) ;
+                print_string " = ";
+                print_int (acc);
+                print_newline ();
+                
+                if n = 0 then acc else match acc mod 2 with
+                    | 0 -> aux (acc/2) (n-1)
+                    | _ -> aux (3*acc+1) (n-1)
+            in aux p n;;
 
-type complexe = {re:float; im:float};;
-let z = {re=3.;im=6.}
-let im_z = z.im
+let c3 = affiche 9 87;;
 
-type complexe2 = Complexe of float * float;;
-let z2 = Complexe (4., 5.9);;
+let tempsdeVol p =
+    let rec aux acc n =
+        if acc = 1 then n else match acc mod 2 with
+            | 0 -> aux (acc/2) (n+1)
+            | _ -> aux (3*acc+1) (n+1)
+    in aux p 0;;
 
+let c4 = tempsdeVol 37;;
 
-let affiche_complexe z = 
-  print_float z.re;
-  print_string " + i";
-  print_float z.im;;
-let c = affiche_complexe z;;
+let hauteur p =
+    let rec aux acc n max_t =
+        if acc = 1 then max_t else match acc mod 2 with
+            | 0 -> aux (acc/2) (n+1) (max acc max_t)
+            | _ -> aux (3*acc+1) (n+1) (max acc max_t)
+    in aux p 0 p;;
 
-exception Complexe_nul;;
+let c5 = hauteur 2049;;
 
-(*
-let inverse z = try 1 /. z.re*.z.re + z.im*.z.im with 
- | Complexe_nul -> failwith "Complexe nul"
- | _ -> {re=z.re /. (z.re*z.re + z.im*z.im) ; im= -. z.im /. (/. z.re*z.re + z.im*z.im)};;
-*)
+let somme p = 
+    let rec aux acc n s =
+        if acc = 1 then (s+1) else match acc mod 2 with
+            | 0 -> aux (acc/2) (n+1) (s+acc)
+            | _ -> aux (3*acc+1) (n+1) (s+acc)
+    in aux p 0 0;;
 
-let inverse2 z = match z.re*.z.re + z.im*.z.im with
- | 0. -> raise Complexe_nul
- | _ -> {re=z.re/.(z.re*.z.re + z.im*.z.im), im=-.z.im/(z.re*.z.re + z.im*.z.im)};;
-(*pas calculer d 3 fois*)
-
-let argument z = match z.re*.z.re + z.im*.z.im with
- | 0. -> raise Complexe_nul
- | _ -> 
-  
+let c6 = somme 2049;;
