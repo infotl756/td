61d0a453b24e3459c2b2bf7f57fb1ae35ffae47b
infotl756 <thoms35bzh@gmail.com>
2026-03-04 21:20:06 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,61 +1,152 @@
-let expo_rapide_terminale x n = 
-    let rec aux acc x n =
-        if n = 0 then acc else match n mod 2 with
-            | 0 -> aux acc (x*x) (n/2)
-            | _ -> aux (acc*x) (x*x) ((n-1)/2)
-    in aux 1 x n;;
+(* TP Ocaml sur les listes *)
 
-let c1 = expo_rapide_terminale 5 3;;
+(* 1 *)
+let nombre_occurences l x = 
+    let rec aux acc l = match l with
+        | [] -> acc
+        | a::q -> 
+            if a = x then
+                aux (acc+1) q 
+            else
+                aux acc q 
+    in aux 0 l;;
+    
+(* 2 - version couteuse *)
+let appliquer f l =
+    let rec aux acc l = match l with 
+        | [] -> acc
+        | a::q -> aux (acc @ f(a)) q
+    in aux [] l;;
 
-let syracuse n p = 
-    let rec aux acc n =
-        if n = 0 then acc else match acc mod 2 with
-            | 0 -> aux (acc/2) (n-1)
-            | _ -> aux (3*acc+1) (n-1)
-    in aux p n;;
+(* 2 - version optimisée, @ parcourt toute la liste à sa gauche d'où (f a) @ acc pour passer de O(n) à O(n^2) *)
+let appliquer f l =
+    let rec aux acc l = match l with
+        | [] -> List.rev acc
+        | a::q -> aux (List.rev (f a) @ acc) q
+    in aux [] l;;
 
-let c2 = syracuse 9 87;;
+(* 3 *)
+let maximum l = match l with 
+    | [] -> raise (Failure "erreur, liste vide")
+    | a::q -> 
+        let rec aux acc l = match l with
+            | [] -> acc
+            | a::q -> 
+                if a > acc then
+                    aux a q
+                else 
+                    aux acc q
+        in aux a l;; 
 
-let affiche n p =
-    let n0 = n in    
-        let rec aux acc n =
-                
-                print_string "Le terme u_";
-                print_int (n0-n) ;
-                print_string " = ";
-                print_int (acc);
-                print_newline ();
-                
-                if n = 0 then acc else match acc mod 2 with
-                    | 0 -> aux (acc/2) (n-1)
-                    | _ -> aux (3*acc+1) (n-1)
-            in aux p n;;
+(* 4 *)
+let dernier l = match l with 
+    | [] -> raise (Failure "erreur, liste vide")
+    | a::q -> 
+        let rec aux acc l = match l with
+            | [] -> acc
+            | a::q -> aux a q
+        in aux a l;;
 
-let c3 = affiche 9 87;;
+(* 5 *)
+let contient e l = match l with 
+    | [] -> raise (Failure "erreur, liste vide")
+    | a::q -> 
+        let rec aux acc l = match l with
+            | [] -> acc
+            | a::q -> 
+                if a = e then
+                    aux true q
+                else 
+                    aux acc q
+        in aux false l;;
+    
+(* 6 *)
+let indice e l = 
+    let rec aux i l = match l with
+        | [] -> raise (Failure "élément non présent")
+        | a::q -> 
+            if a = e then i
+            else aux (i+1) q
+    in aux 0 l;;
 
-let tempsdeVol p =
-    let rec aux acc n =
-        if acc = 1 then n else match acc mod 2 with
-            | 0 -> aux (acc/2) (n+1)
-            | _ -> aux (3*acc+1) (n+1)
-    in aux p 0;;
+(* 7 *)
+let liste_indice e l =
+    let rec aux acc i l = match l with
+        | [] -> 
+            if acc = [] then raise (Failure "élément non présent")
+            else acc
+        | a::q -> 
+            if a = e then aux (i::acc) (i+1) q
+            else aux acc (i+1) q
+    in aux [] 0 l;;
 
-let c4 = tempsdeVol 37;;
+(* 8 *)
+let nieme n l = 
+    let rec aux i l = match l with
+        | [] -> raise (Failure "index error")
+        | a::q -> 
+            if i = n then a
+            else aux (i+1) q
+    in aux 0 l;;
 
-let hauteur p =
-    let rec aux acc n max_t =
-        if acc = 1 then max_t else match acc mod 2 with
-            | 0 -> aux (acc/2) (n+1) (max acc max_t)
-            | _ -> aux (3*acc+1) (n+1) (max acc max_t)
-    in aux p 0 p;;
+(* 9 *)
+let rec pour_tout p l = match l with
+    | [] -> true
+    | a::q -> 
+        if p a = true then pour_tout p q
+        else false;;
 
-let c5 = hauteur 2049;;
+(* 10 *)
+let rec existe p l = match l with
+    | [] -> false
+    | a::q -> 
+        if p a then true
+        else existe p q;;
 
-let somme p = 
-    let rec aux acc n s =
-        if acc = 1 then (s+1) else match acc mod 2 with
-            | 0 -> aux (acc/2) (n+1) (s+acc)
-            | _ -> aux (3*acc+1) (n+1) (s+acc)
-    in aux p 0 0;;
+(* 11 *)
+let rec trouver p l = match l with
+    | [] -> raise (Failure "Not Found")
+    | a::q -> 
+        if p a then a 
+        else trouver p q;;
 
-let c6 = somme 2049;;
+(* 12 , attention l'ordre de renvoi est inversé*)
+let filtre p l = 
+    let rec aux acc l = match l with
+        | [] -> acc
+        | a::q -> 
+            if p a then aux (a::acc) q
+            else aux acc q
+    in aux [] l;; 
+
+(* 13 , attention l'ordre de renvoi est inversé*)
+let partition p l =
+    let rec aux (acc1, acc2) l = match l with
+        | [] -> (acc1, acc2)
+        | a::q -> 
+            if p a then aux (a::acc1, acc2) q
+            else aux (acc1, a::acc2) q
+    in aux ([], []) l;;
+
+(* 14 , ici l'ordre de renvoi n'est pas inversé*)
+let separer l =
+    let rec aux (acc1, acc2) l = match l with
+        | [] -> (List.rev acc1, List.rev acc2)
+        | (a,b)::q -> aux ((a::acc1), (b::acc2)) q
+    in aux ([], []) l;;
+
+(* 15 *)
+let iterees a n f = 
+    let rec aux acc l i = 
+        if i = n then l
+        else aux (f acc) (f acc::l) (i+1)
+    in aux a [a] 0;;
+
+(* 16 *)
+let entiersC n =
+    let rec aux k l = 
+        if k = 0 then 0::l
+        else aux (k-1) (k::l)
+    in aux n [];;
+
+let l1 = entiersC 10;;
