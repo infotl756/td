f3dc1a83d6404931aaac7114bd668f97371bfa8c
infotl756 <thoms35bzh@gmail.com>
2026-03-16 00:49:17 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,183 +1,42 @@
-
-(* TP 1 Ocaml *)
-
-let tempsdeVol p =
-    let rec aux acc n =
-        if acc = 1 then n else match acc mod 2 with
-            | 0 -> aux (acc/2) (n+1)
-            | _ -> aux (3*acc+1) (n+1)
-    in aux p 0;;
-
-let c4 = tempsdeVol 37;;
-
-let hauteur p =
-    let rec aux acc n max_t =
-        if acc = 1 then max_t else match acc mod 2 with
-            | 0 -> aux (acc/2) (n+1) (max acc max_t)
-            | _ -> aux (3*acc+1) (n+1) (max acc max_t)
-    in aux p 0 p;;
-
-let c5 = hauteur 2049;;
-
-let somme p = 
-    let rec aux acc n s =
-        if acc = 1 then (s+1) else match acc mod 2 with
-            | 0 -> aux (acc/2) (n+1) (s+acc)
-            | _ -> aux (3*acc+1) (n+1) (s+acc)
-    in aux p 0 0;;
-
-let c6 = somme 2049;;
+let produit_cartesien a b = 
+    let rec aux acc1 acc2 l =
+        match (acc1, acc2) with
+            | ([], t::q) -> aux a q l
+            | (t::q, []) -> l
+            | ([],[]) -> l 
+            | (t1::q1, t2::q2) -> aux q1 (t2::q2) ((t1, t2)::l)
+    in aux a b [];;
 
 
-(* *)
+let iteration_droite f l b = 
+    match l with
+        |[] -> b
+        |t :: q -> 
+            let rec aux acc l = match l with
+                | [] -> acc
+                | t::q -> aux (t*(f acc)) q
+            in aux (t*(f b)) q;;
 
-(* 1 *)
-let nombre_occurences l x = 
-    let rec aux acc l = match l with
-        | [] -> acc
-        | a::q -> 
-            if a = x then
-                aux (acc+1) q 
-            else
-                aux acc q 
-    in aux 0 l;;
+let somme = iteration_droite (+) l 0;;
+
+let iteration_gauche f l a =
+    match l with
+        |[] -> a
+        |t :: q -> 
+            let rec aux acc l = match l with
+                | [] -> acc
+                | t::q -> aux (f acc t) q
+            in aux (f a t) q;;
+
+let ajoute_tete acc t = t::acc;;
+
+let renverser l = iteration_gauche ajoute_tete l [];;
+
+let rec insere x l = match l with
+    | [] -> [x]
+    | t::q -> 
+        if x <= t then x::l
+        else t::(insere x q);;
+
+let tri_insertion = iteration_droite insere l [];;
     
-(* 2 - version couteuse *)
-let appliquer f l =
-    let rec aux acc l = match l with 
-        | [] -> acc
-        | a::q -> aux (acc @ f(a)) q
-    in aux [] l;;
-
-(* 2 - version optimisée, @ parcourt toute la liste à sa gauche d'où (f a) @ acc pour passer de O(n) à O(n^2) *)
-let appliquer f l =
-    let rec aux acc l = match l with
-        | [] -> List.rev acc
-        | a::q -> aux (List.rev (f a) @ acc) q
-    in aux [] l;;
-
-(* 3 *)
-let maximum l = match l with 
-    | [] -> raise (Failure "erreur, liste vide")
-    | a::q -> 
-        let rec aux acc l = match l with
-            | [] -> acc
-            | a::q -> 
-                if a > acc then
-                    aux a q
-                else 
-                    aux acc q
-        in aux a l;; 
-
-(* 4 *)
-let dernier l = match l with 
-    | [] -> raise (Failure "erreur, liste vide")
-    | a::q -> 
-        let rec aux acc l = match l with
-            | [] -> acc
-            | a::q -> aux a q
-        in aux a l;;
-
-(* 5 *)
-let contient e l = match l with 
-    | [] -> raise (Failure "erreur, liste vide")
-    | a::q -> 
-        let rec aux acc l = match l with
-            | [] -> acc
-            | a::q -> 
-                if a = e then
-                    aux true q
-                else 
-                    aux acc q
-        in aux false l;;
-    
-(* 6 *)
-let indice e l = 
-    let rec aux i l = match l with
-        | [] -> raise (Failure "élément non présent")
-        | a::q -> 
-            if a = e then i
-            else aux (i+1) q
-    in aux 0 l;;
-
-(* 7 *)
-let liste_indice e l =
-    let rec aux acc i l = match l with
-        | [] -> 
-            if acc = [] then raise (Failure "élément non présent")
-            else acc
-        | a::q -> 
-            if a = e then aux (i::acc) (i+1) q
-            else aux acc (i+1) q
-    in aux [] 0 l;;
-
-(* 8 *)
-let nieme n l = 
-    let rec aux i l = match l with
-        | [] -> raise (Failure "index error")
-        | a::q -> 
-            if i = n then a
-            else aux (i+1) q
-    in aux 0 l;;
-
-(* 9 *)
-let rec pour_tout p l = match l with
-    | [] -> true
-    | a::q -> 
-        if p a = true then pour_tout p q
-        else false;;
-
-(* 10 *)
-let rec existe p l = match l with
-    | [] -> false
-    | a::q -> 
-        if p a then true
-        else existe p q;;
-
-(* 11 *)
-let rec trouver p l = match l with
-    | [] -> raise (Failure "Not Found")
-    | a::q -> 
-        if p a then a 
-        else trouver p q;;
-
-(* 12 , attention l'ordre de renvoi est inversé*)
-let filtre p l = 
-    let rec aux acc l = match l with
-        | [] -> acc
-        | a::q -> 
-            if p a then aux (a::acc) q
-            else aux acc q
-    in aux [] l;; 
-
-(* 13 , attention l'ordre de renvoi est inversé*)
-let partition p l =
-    let rec aux (acc1, acc2) l = match l with
-        | [] -> (acc1, acc2)
-        | a::q -> 
-            if p a then aux (a::acc1, acc2) q
-            else aux (acc1, a::acc2) q
-    in aux ([], []) l;;
-
-(* 14 , ici l'ordre de renvoi n'est pas inversé*)
-let separer l =
-    let rec aux (acc1, acc2) l = match l with
-        | [] -> (List.rev acc1, List.rev acc2)
-        | (a,b)::q -> aux ((a::acc1), (b::acc2)) q
-    in aux ([], []) l;;
-
-(* 15 *)
-let iterees a n f = 
-    let rec aux acc l i = 
-        if i = n then l
-        else aux (f acc) (f acc::l) (i+1)
-    in aux a [a] 0;;
-
-(* 16 *)
-let entiersC n =
-    let rec aux k l = 
-        if k = 0 then 0::l
-        else aux (k-1) (k::l)
-    in aux n [];;
-
-let l1 = entiersC 10;;
