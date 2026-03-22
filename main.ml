e3907972750e92234631663bf473eef40c55c69a
infotl756 <thoms35bzh@gmail.com>
2026-03-23 00:40:51 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,42 +1,25 @@
-let produit_cartesien a b = 
-    let rec aux acc1 acc2 l =
-        match (acc1, acc2) with
-            | ([], t::q) -> aux a q l
-            | (t::q, []) -> l
-            | ([],[]) -> l 
-            | (t1::q1, t2::q2) -> aux q1 (t2::q2) ((t1, t2)::l)
-    in aux a b [];;
+let rec est_normal p = match p with
+| [] -> true
+| [t] -> if t = 0. then false 
+         else true
+| t::q -> est_normal q
 
+let dilatation p a =
+  let rec aux acc l = match l with
+    | [] -> List.rev acc
+    | t::q -> aux ((a *. t) :: acc) q
+  in aux [] p
 
-let iteration_droite f l b = 
-    match l with
-        |[] -> b
-        |t :: q -> 
-            let rec aux acc l = match l with
-                | [] -> acc
-                | t::q -> aux (t*(f acc)) q
-            in aux (t*(f b)) q;;
+  let somme p1 p2 =
+    let rec aux acc l1 l2 = match (l1, l2) with
+        | ([], []) -> List.rev acc
+        | (t::q, []) -> aux (t::acc) q []
+        | ([], t::q) -> aux (t::acc) [] q
+        | (t1::q1, t2::q2) -> aux ((t1 +. t2)::acc) q1 q2
+    in aux [] p1 p2
 
-let somme = iteration_droite (+) l 0;;
+let evaluation p v =
+    let rec aux acc l = match l with
+        | [] -> acc
+        | t::q -> 
 
-let iteration_gauche f l a =
-    match l with
-        |[] -> a
-        |t :: q -> 
-            let rec aux acc l = match l with
-                | [] -> acc
-                | t::q -> aux (f acc t) q
-            in aux (f a t) q;;
-
-let ajoute_tete acc t = t::acc;;
-
-let renverser l = iteration_gauche ajoute_tete l [];;
-
-let rec insere x l = match l with
-    | [] -> [x]
-    | t::q -> 
-        if x <= t then x::l
-        else t::(insere x q);;
-
-let tri_insertion = iteration_droite insere l [];;
-    
